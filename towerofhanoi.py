11498700e53a04cf1e10d961c584efa0368dc128
Mohammad Shariq Pir <pirshariq419@gmail.com>
2025-06-12 01:01:45 +0530

    Update towerofhanoi.py

--- towerofhanoi.py
+++ towerofhanoi.py
@@ -1,10 +1,58 @@
-def tower_of_hanoi(num, source, aux, target):
-    if num == 1:
-        print(f"Move disk 1 from {source} to {target}")
-        return
-    tower_of_hanoi(num - 1, source, target, aux)
-    print(f"Move disk {num} from {source} to {target}")
-    tower_of_hanoi(num - 1, aux, source, target)
+def tower_of_hanoi(n, source, helper, target, pegs):
 
-num_disks = 3
-tower_of_hanoi(num_disks, "A", "B", "C")
+    if n == 1:
+
+        disk = pegs[source].pop()
+
+        pegs[target].append(disk)
+
+        print(f"Move disk {disk} from {source} to {target}")
+
+        return
+
+
+
+    tower_of_hanoi(n - 1, source, target, helper, pegs)
+
+    disk = pegs[source].pop()
+
+    pegs[target].append(disk)
+
+    print(f"Move disk {disk} from {source} to {target}")
+
+    tower_of_hanoi(n - 1, helper, source, target, pegs)
+
+
+
+n = 4
+
+pegs = {
+
+    'A': [3,2,1,0],
+
+    'B': [],
+
+    'C': []
+
+}
+
+
+
+print("\nInitial Pegs State:")
+
+for peg in 'ABC':
+
+    print(f"{peg}: {pegs[peg]}")
+
+
+
+tower_of_hanoi(n, 'A', 'B', 'C', pegs)
+
+
+
+print("\nFinal Pegs State:")
+
+for peg in 'ABC':
+
+    print(f"{peg}: {pegs[peg]}")
+
